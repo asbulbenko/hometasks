22df07622e7d7f06d3f729143b2aa013002ee282
oleksiibulbenko <abulbenko@slice.com>
2019-02-06 22:52:08 +0200

    Added handled incorrect input exception. Added divide by zero case.

--- Calculator/src/MainClass.java
+++ Calculator/src/MainClass.java
@@ -1,42 +1,53 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MainClass {
 
     public static void main(String[] args){
-        int number1,number2,summ;
+        double number1,number2;
 
         Scanner in = new Scanner(System.in);
 
-        // input number1 from console
-        System.out.println("Input first number");
-        number1 = in.nextInt();
+        try {
+            // input number1 from console
+            System.out.println("Input first number");
+            number1 = in.nextDouble();
 
-        // input number2 from console
-        System.out.println("Input second number");
-        number2 = in.nextInt();
+            // input number2 from console
+            System.out.println("Input second number");
+             number2 = in.nextDouble();
 
         // input operation
         System.out.println("Input operation symbol +, -, *, / or %");
 
         switch (in.next().charAt(0)){
             case '+':
-                System.out.println("Addition result = " + (number1+number2));
+                System.out.println("Addition result = " + (number1 + number2));
                 break;
             case '-':
-                System.out.println("Subtraction result = " + (number1-number2));
+                System.out.println("Subtraction result = " + (number1 - number2));
                 break;
             case '*':
-                System.out.println("Multiplication result = " + (number1*number2));
+                System.out.println("Multiplication result = " + (number1 * number2));
                 break;
             case '/':
-                System.out.println("Devide result = " + (number1/number2));
+                if (number2 == 0) {
+                    System.out.println("You cannot divide by zero");
+                } else
+                    System.out.println("Divide result = " + (number1 / number2));
                 break;
             case '%':
-                System.out.println("Module result = " + (number1%number2));
+                System.out.println("Module result = " + (number1 % number2));
                 break;
             default:
                 System.out.println("Incorrect symbol");
         }
+        } catch (InputMismatchException e){
+            System.out.println("You entered incorrect data for number");
+        } catch (Exception e){
+            System.out.println("Exception occurred");
+        }
+        System.out.println("Please run program again");
 
         // close console input
         in.close();
